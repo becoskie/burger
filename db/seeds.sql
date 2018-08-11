4b4ca9748a1c166c6cb2b538c9041337be853f70
Ray Becoskie <ray@becoskie.com>
2018-08-11 04:46:15 -0500

    Basic working structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkeyburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggieburger', false);
---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
+var app = express();
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+var routes = require("./controllers/burger_controller.js");
+// Serve index.handlebars to the root route.
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
